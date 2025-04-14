4ce33ee8c07a20a563b437dfc0003fb2959eb4d5
lunginspector <138542747+lunginspector@users.noreply.github.com>
2025-04-14 16:09:33 -0700

    Revert "may fix the resolution setter; again do not use ts"
    This reverts commit f5b2c150c745ff82fff96b2cd649421ee102fb8e.

--- Nugget/Controllers/Tweaks/MobileGestaltManager.swift
+++ Nugget/Controllers/Tweaks/MobileGestaltManager.swift
@@ -52,11 +52,31 @@ class MobileGestaltManager {
     }
     
     func setGestaltValue(key: String, value: Any) {
-        if let dataValue = value as? Data {
-            self.GestaltChanges[key] = dataValue
-        } else {
-            self.GestaltChanges[key] = value
-        }
+//        let gestaltURL = URL.documents.appendingPathComponent("com.apple.MobileGestalt.plist")
+//        let stringsData = try Data(contentsOf: gestaltURL)
+//        
+//        // open plist
+//        let plist = try! PropertyListSerialization.propertyList(from: stringsData, options: [], format: nil) as! [String: Any]
+//        func changeDictValue(_ dict: [String: Any], _ key: String, _ value: Any) -> [String: Any] {
+//            var newDict = dict
+//            for (k, v) in dict {
+//                if k == key {
+//                    newDict[k] = value
+//                } else if let subDict = v as? [String: Any] {
+//                    newDict[k] = changeDictValue(subDict, key, value)
+//                }
+//            }
+//            return newDict
+//        }
+//        
+//        // modify value
+//        var newPlist = plist
+//        newPlist = changeDictValue(newPlist, key, value)
+//        
+//        // overwrite the plist
+//        let newData = try PropertyListSerialization.data(fromPropertyList: newPlist, format: .binary, options: 0)
+//        try newData.write(to: gestaltURL)
+        self.GestaltChanges[key] = value
     }
     func setGestaltValues(keys: [String], values: [Any]) {
         for keyIdx in 0...(keys.count - 1) {
